a15273afdce9400c1b11bbe12be6cf23494733d2
nexstack-AI <xudongdong502@gmail.com>
2026-05-19 23:24:23 +0800

    feat: translate easter egg console messages to Chinese, change trigger name to 东哥

--- src/components/easter-eggs.tsx
+++ src/components/easter-eggs.tsx
@@ -20,33 +20,33 @@ const EasterEggs = () => {
     if (typeof console !== "undefined") {
       console.clear();
       console.log(
-        "%cWhoa, look at you! 🕵️‍♂️\n" +
-          "You seem to have discovered the secret console! 🔍\n" +
-          "Want to see some magic? ✨\n" +
-          "Just type %cmy first name%c and hit enter! 🎩🐇",
+        "%c哇哦，被你发现了！🕵️‍♂️\n" +
+          "看来你找到了秘密控制台！🔍\n" +
+          "想看点魔法吗？✨\n" +
+          "在下面输入 %c东哥%c 然后回车试试！🎩🐇",
         //   "Just press the %c'n'%c key and watch the magic happen! 🪄",
         "color: #FFD700; font-size: 16px; font-weight: bold; background-color: black; padding: 10px; border-radius: 10px; margin-top:20px",
         "color: #00FF00; font-size: 16px; font-weight: bold; background-color: black; padding: 10px; border-radius: 10px; margin-top:20px",
         "color: #FFD700; font-size: 16px; font-weight: bold; background-color: black; padding: 10px; border-radius: 10px;"
       );
 
-      ["naresh", "Naresh", "NARESH"].forEach((name) => {
+      ["东哥", "dongge", "Dongge"].forEach((name) => {
         // @ts-ignore
         if (Object.hasOwn(window, name)) return;
         Object.defineProperty(window, name, {
           get() {
             console.log(
-              "%c✨ Abra Kadabra! ✨\n\n" +
-                "You just summoned the magic of Naresh! 🧙‍♂️\n" +
-                "What??? youre not impressed? Fine, but remember: With great power comes great responsibility! 💻⚡",
+              "%c✨ 变！✨\n\n" +
+                "你成功召唤了东哥的魔法！🧙‍♂️\n" +
+                "什么？？你居然不觉得厉害？好吧，但记住：能力越大，责任越大！💻⚡",
 
               "color: #FF4500; font-size: 18px; font-weight: bold; background-color: black; padding: 10px; border-radius: 10px; margin-top:10px"
             );
 
             const timer = setTimeout(() => {
               console.log(
-                "%cPssttt! 🤫\n\n" +
-                  "Do you like cats?? 😺 If yes, then press 'n' on viewport and see what happens! 🐱✨",
+                "%c嘘！🤫\n\n" +
+                  "你喜欢猫吗？？😺 喜欢的话，在页面上按 'n' 键看看会发生什么！🐱✨",
                 "color: #FF69B4; font-size: 16px; font-weight: bold; background-color: black; padding: 10px; border-radius: 10px;"
               );
               clearTimeout(timer);
